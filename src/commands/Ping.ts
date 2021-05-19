5021133337dc9188c024e119cc40b269f6547451
Carsten Daurehøj <arelstone@gmail.com>
2021-05-19 12:56:00 +0200

    Cleanup + better debug logging

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -4,9 +4,7 @@ import { Command, CommandMessage, Description } from '@typeit/discord';
 export default abstract class Ping {
   @Command('ping')
   @Description('Pong')
-    async execute(command: CommandMessage) {
-        console.log('Command: Ping');
-        
+    async execute(command: CommandMessage) {        
         return await command.reply('Pong!');
     }
 }
