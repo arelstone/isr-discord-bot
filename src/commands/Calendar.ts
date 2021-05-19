a669ce5dca37909f89469367c521e14720cc379b
Carsten Daurehøj <arelstone@gmail.com>
2021-05-19 16:16:48 +0200

    add try catch

--- src/commands/Calendar.ts
+++ src/commands/Calendar.ts
@@ -5,21 +5,26 @@ import { calendarEventsFields } from './utils/calendarEventFields';
 export default abstract class Calendar {
     private events(command: CommandMessage) {
         return calendarEventsFields(command);
+        
     }
 
     @Command('calendar')
     @Description('Wanna know when we have events?')
     async execute(command: CommandMessage){
-        return await command.channel.send({
-            embed: {
-                title: `${command?.guild?.name ?? 'Our'} event calendar`,
-                description: `**We love racing.**
+        try {
+            return await command.channel.send({
+                embed: {
+                    title: `${command?.guild?.name ?? 'Our'} event calendar`,
+                    description: `**We love racing.**
                 
 Our event calendar is as follows.
 Find the event suited for you, join channel and start racing`,
-                files: [new MessageAttachment('./src/assets/cover-image.png')],
-                fields: await this.events(command),
-            },
-        });
+                    files: [new MessageAttachment('./src/assets/cover-image.png')],
+                    fields: await this.events(command),
+                },
+            });
+        } catch (error) {
+            return await command.reply(`Could not generate event calendar, Error: ${error.message}`);
+        }
     }
 }
