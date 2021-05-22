e1512619ff53434b98223f4d5192bef90a3d8216
Carsten Daurehøj <arelstone@gmail.com>
2021-05-22 10:19:00 +0200

    get channels

--- src/commands/Channels.ts
+++ src/commands/Channels.ts
@@ -4,7 +4,11 @@ export default abstract class Channels {
     @Command('channels')
     @Infos({description: 'Get a list of all channels', hidden: true})
     async execute(command: CommandMessage) {
-        const channels = command.guild.channels.cache.map(channel => ({id: channel.id, name: channel.name}));
+        // console.log(command.client.channels);
+        const channels = command.client.channels.cache.filter(c => c.isText());
+
+        console.log(channels);
+        
         
         return await command.reply(JSON.stringify(channels, null, 2));
     }
